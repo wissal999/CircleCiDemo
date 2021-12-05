4c3971f65336ec91abe00620e815d055444b83e7
Wissal <ouissaltouaty99@gmail.com>
2021-12-05 10:44:44 +0000

    test

---
+++ app/src/main/java/com/example/circlecidemo/Validator.java
@@ -0,0 +1,10 @@
+package com.example.circlecidemo;
+
+public class Validator {
+
+    public  long Somme( long valeur1, long valeur2) {
+
+        return valeur1+valeur2;
+
+    }
+}
---
+++ app/src/test/java/com/example/circlecidemo/ValidatorTest.java
@@ -0,0 +1,18 @@
+package com.example.circlecidemo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ValidatorTest {
+
+
+    private final Validator validator=new Validator();
+
+
+    @Test
+    public void Somme()
+    {
+        assertEquals(9,validator.Somme(5,4));
+    }
+}
