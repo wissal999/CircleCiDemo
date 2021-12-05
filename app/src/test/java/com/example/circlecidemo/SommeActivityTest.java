3cb4aecf0d3705725db864b586809a89a251ae29
Wissal <ouissaltouaty99@gmail.com>
2021-12-05 10:09:51 +0000

    succes test

--- app/src/test/java/com/example/circlecidemo/SommeActivityTest.java
+++ app/src/test/java/com/example/circlecidemo/SommeActivityTest.java
@@ -7,6 +7,7 @@ import org.junit.runners.JUnit4;
 import static org.junit.Assert.*;
 @RunWith(JUnit4.class)
 public class SommeActivityTest {
+
     private final SommeActivity Somme=new SommeActivity();
     @Test
     public void somme() {
