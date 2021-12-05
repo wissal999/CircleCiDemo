a0d20b32e8ad7218164ea052474e1d5528e8e173
Wissal <ouissaltouaty99@gmail.com>
2021-12-05 13:39:06 +0000

    commit

--- app/src/androidTest/java/com/example/circlecidemo/SommeActivityTest.java
+++ app/src/androidTest/java/com/example/circlecidemo/SommeActivityTest.java
@@ -6,6 +6,7 @@ import androidx.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,11 +15,16 @@ public class SommeActivityTest {
     private SommeActivity sommeActivity;
     @Before
     public void setUp() throws Exception {
-        sommeActivity=null;
+       sommeActivity=activityTestRule.getActivity();
     }
 
     @After
     public void tearDown() throws Exception {
+        sommeActivity=null;
+    }
+    @Test
+    public void Somme()
+    {
         assertEquals(9,sommeActivity.Somme(5,4));
     }
 }
