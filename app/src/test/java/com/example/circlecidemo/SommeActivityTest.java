9618a796735f102d7330b7c181ad55bd220bc88e
Wissal <ouissaltouaty99@gmail.com>
2021-12-05 13:12:56 +0000

    comiit110

--- app/src/test/java/com/example/circlecidemo/SommeActivityTest.java
+++ app/src/test/java/com/example/circlecidemo/SommeActivityTest.java
@@ -1,16 +1,29 @@
 package com.example.circlecidemo;
 
+import androidx.test.rule.ActivityTestRule;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class SommeActivityTest {
 
-    private  SommeActivity sommeActivity=new SommeActivity();
+  @Rule
+  public ActivityTestRule<SommeActivity>activityTestRule=new ActivityTestRule<SommeActivity>(SommeActivity.class);
+  private SommeActivity sommeActivity=null;
 
+    @Before
+    public void setUp() throws Exception {
+        sommeActivity=activityTestRule.getActivity();
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        sommeActivity=null;
+    }
     @Test
     public void somme() {
         assertEquals(9,sommeActivity.Somme(5,4));
